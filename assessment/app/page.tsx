549b677d2bb6d73705cc7ec249b674c2997692ae
xinhan <xhxhan00@gmail.com>
2026-03-07 13:14:47 +0800

    Give a Main as Div, added Navbar

--- assessment/app/page.tsx
+++ assessment/app/page.tsx
@@ -1,7 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <main>
+    <Navbar/>
     <h1>HELLO WORLD</h1>
+    </main>
   )
 }
