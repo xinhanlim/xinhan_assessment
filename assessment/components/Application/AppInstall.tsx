deda0bfae04a8e588f37df259b077a23e2f50b29
xinhan <xhxhan00@gmail.com>
2026-03-07 18:12:22 +0800

    temporary added a section to have the install icons

---
+++ assessment/components/Application/AppInstall.tsx
@@ -0,0 +1,12 @@
+export default function AppInstall() {
+    return (
+        <section id="appinstall" className="flex px-[5%] justify-center ">
+            <div className="w-[1530px]  ">
+                <h2>Goolge Install</h2>
+                <h2>Apple Install</h2>
+            </div>
+        </section>
+    )
+
+
+}
