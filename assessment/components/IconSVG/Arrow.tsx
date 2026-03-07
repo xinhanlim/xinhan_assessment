80e763bdae769534728541696ef4712b4737013a
xinhan <xhxhan00@gmail.com>
2026-03-07 14:28:20 +0800

    added animation for arrow for register

---
+++ assessment/components/IconSVG/Arrow.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+type RightArrowProps = {
+  currentColor?: string;
+};
+
+export default function RightArrow({ currentColor = 'currentColor' }: RightArrowProps) {
+  return (
+    <svg
+      className="transform -rotate-90 group-hover:-rotate-45 transition-transform duration-300"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill={currentColor}
+    >
+      <path d="M17 15.586 6.707 5.293 5.293 6.707 15.586 17H7v2h12V7h-2v8.586z" />
+    </svg>
+  );
+}
