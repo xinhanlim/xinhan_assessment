52e86cc6cce599c59c94e4134fc58fd8f207f4cc
xinhan <xhxhan00@gmail.com>
2026-03-07 18:41:49 +0800

    change h2 to h1 for hero  header

--- assessment/components/Hero/Hero.tsx
+++ assessment/components/Hero/Hero.tsx
@@ -8,10 +8,10 @@ export default function Hero() {
     return (
         <section id="hero" className="relative flex justify-center px-[5%] w-full h-full">
             <div className=" flex flex-col sm:flex-row w-full xl:w-[1530px] h-[80vh] sm:h-screen flex py-[60px] md:pt-[150px] justify-between ">
-                <div className="flex flex-col md:flex-col tracking-tighter leading-none gap-4 max-w-[700px]">
-                    <h2 className="z-10 text-[32px] md:text-[64px] tracking-tighter leading-none uppercase">
+                <div className="relative flex flex-col md:flex-col tracking-tighter leading-none gap-4 max-w-[700px]">
+                    <h1 className="z-10 text-[32px] md:text-[64px] tracking-tighter leading-none uppercase">
                         COPY TRADING WITH BACKWELL INVEST
-                    </h2>
+                    </h1>
                 </div>
                 <div className="absolute inset-0 z-0 overflow-hidden">
                     <Image
@@ -25,9 +25,20 @@ export default function Hero() {
 
                 <div className="flex flex-col gap-20 justify-end">
                     <div className="z-10 flex flex-col md:flex-col tracking-tighter leading-none gap-4 max-w-[700px]">
-                        <h2 className=" text-[32px] md:text-[64px] tracking-tighter leading-none uppercase ">
-                            Choose & TradeReady-To-Go Strategies
-                        </h2>
+
+                        <h1 className=" text-[32px] md:text-[64px] tracking-tighter leading-none uppercase ">
+                            Choose & TradeReady-To-Go 
+                            <span className="relative inline-block gap-2">
+                                Strategies
+                                <Image
+                                    src="/images/regular.png"
+                                    alt="photo of regular"
+                                    width={80}
+                                    height={80}
+                                    className="absolute -right-20 top-10 -translate-y-1/2 rotate-[-25deg]"
+                                />
+                            </span>
+                        </h1>
                         <div className="flex flex-col gap-8">
                             <p className="sm:text-[18px]">Browse and copy hundreds of investment strategies developed by master traders! Whether you are a pro or beginner, you can now trade quicker and more confidently.</p>
                             <div className="flex flex-row flex-wrap gap-2">
@@ -36,6 +47,7 @@ export default function Hero() {
                                 <ClassLabels name="Oil" color="#fefefe" />
                                 <ClassLabels name="Indices" color="#fefefe" />
                             </div>
+
                         </div>
 
 
