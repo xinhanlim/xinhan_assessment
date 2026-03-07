8c9d78a01fa38a1cba6e14ebfbeccf88d1cf745e
xinhan <xhxhan00@gmail.com>
2026-03-07 18:42:06 +0800

    change p to h5

--- assessment/ultis/PromotionCounter/Counter.tsx
+++ assessment/ultis/PromotionCounter/Counter.tsx
@@ -8,8 +8,8 @@ interface CounterProps {
 export default function Counter({amount,format}:CounterProps){
     return (
         <div className="flex flex-col gap-2 items-center justify-between">   
-            <p>{amount}</p>
-            <p>{format}</p>
+            <h5>{amount}</h5>
+            <h5>{format}</h5>
         </div>
     )
 }
