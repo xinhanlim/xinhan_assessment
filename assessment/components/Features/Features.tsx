c1e9d866a0b9595482348e0aa51f626cde19ca98
xinhan <xhxhan00@gmail.com>
2026-03-08 19:32:06 +0800

    reallign spacing

--- assessment/components/Features/Features.tsx
+++ assessment/components/Features/Features.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 export default function Features() {
     return (
-        <section id="features" className="flex justify-center px-[5%] w-full h-full flex-col items-center   ">
-            <div className="flex flex-col sm:flex-row w-full xl:w-[1520px] sm:h-screen flex py-[60px] md:pt-[150px] justify-between ">
+        <section id="features" className="flex justify-center px-[5%] w-full h-full flex-col items-center py-20  gap-20 ">
+            <div className="flex flex-col xl:flex-row w-full xl:max-w-[1520px] sm:h-full sm:py-20 justify-between ">
                 <div className="flex flex-col gap-[60px] w-full h-full">
                     <h1 className=" text-[24px] md:text-[64px] tracking-tighter leading-none">FEATURES - FAST MATCHING</h1>
                     <div className="flex flex-col sm:flex-row justify-between gap-8 md:gap-[120px]">
@@ -19,38 +19,38 @@ export default function Features() {
                             ></video>
                         </div>
                         <div className="w-full h-full">
-                            <ul className="flex flex-col justify-between h-full tracking-tighter leading-none">
-                                <li className="py-8 md:pb-8 md:pt-0 flex flex-row gap-2 ">
+                            <ul className="flex flex-col gap-3 h-full tracking-tighter leading-none">
+                                <li className="pb-6 flex flex-row gap-2 ">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         Spotlight
                                     </p>
                                 </li>
-                                <li className="py-8 md:pb-8 md:pt-0 flex flex-row gap-2">
+                                <li className="py-6 flex flex-row gap-2">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         Top Strategies
                                     </p>
                                 </li>
-                                <li className="py-8 md:pb-8 md:pt-0  flex flex-row gap-2">
+                                <li className="py-6  flex flex-row gap-2">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         Low Drawdown
                                     </p>
                                 </li>
-                                <li className="py-8 md:pb-8 md:pt-0 flex flex-row gap-2">
+                                <li className="py-6 flex flex-row gap-2">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         Medium Drawdown
                                     </p>
                                 </li>
-                                <li className="py-8 md:pb-8 md:pt-0 flex flex-row gap-2">
+                                <li className="py-6 flex flex-row gap-2">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         High Drawdown
                                     </p>
                                 </li>
-                                <li className="py-8 md:pb-8 md:pt-0 flex flex-row gap-2">
+                                <li className="py-6  flex flex-row gap-2">
                                     <Image src="/images/list-icon.png" alt="image of list icon" width={24} height={24} className="object-contain" />
                                     <p>
                                         New Strategie
@@ -61,15 +61,15 @@ export default function Features() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col sm:flex-row w-full xl:w-[1520px] sm:h-screen flex py-[60px] justify-between ">
-                <div className="flex flex-col gap-[60px] w-full h-full">
+            <div className="flex flex-col sm:flex-row w-full xl:w-[1520px] sm:h-full flex justify-between ">
+                <div className="flex flex-col  gap-[60px]  w-full h-full">
                     <h1 className=" text-[24px] md:text-[64px] tracking-tighter leading-none ">FEATURES - EASY ANALYSIS</h1>
                     <div className="flex flex-col-reverse sm:flex-row justify-between gap-[60px]">
                         <div className="w-full h-full">
                             <p>Instant clarity on the Masters’ profile. Get a snapshot of their trade history, profitability, risk, and portfolio all in one place. </p>
                         </div>
                         <div className="relative w-full h-[500px] overflow-hidden border-1 border-white py-8 px-8 videoBg ">
-                            <Image src="/images/functions/mobile-2-new.png" alt="function1" fill className="w-full h-full object-contain" />
+                            <Image src="/images/features/mobile-2-new.png" alt="mobile-2-new" loading="lazy" fill className="w-full h-full object-contain" />
                         </div>
                     </div>
                 </div>
--- assessment/components/Hero/Hero.tsx
+++ assessment/components/Hero/Hero.tsx
@@ -8,9 +8,9 @@ import RightArrow from "../IconSVG/Arrow"
 export default function Hero() {
     return (
         <section id="hero" className="relative flex justify-center px-[5%] w-full h-full">
-            <div className=" flex flex-col sm:flex-row w-full xl:w-[1520px] h-[900px] sm:h-[1000px] flex py-[60px] md:pt-[150px] justify-between ">
+            <div className=" flex flex-col xl:flex-row w-full xl:w-[1520px] h-[900px] lg:h-[1000px] flex py-[60px] xl:pt-[150px] justify-between ">
                 <div className="relative flex flex-col md:flex-col tracking-tighter leading-none gap-4 max-w-[700px]">
-                    <h1 className="z-10 text-[32px] md:text-[64px] tracking-tighter leading-none uppercase">
+                    <h1 className="z-10 text-[32px] sm:text-[50px] md:text-[64px] tracking-tighter leading-none uppercase">
                         COPY TRADING WITH BACKWELL INVEST
                     </h1>
                 </div>
@@ -18,25 +18,25 @@ export default function Hero() {
                     <Image
                         src="/images/hand.png"
                         alt="image of product"
+                        loading="lazy"
                         fill
                         className="object-contain object-top scale-85 overflow-hidden pointer-events-none"
                         style={{ objectPosition: "40% 20%" }}
                     />
                 </div>
-
                 <div className="flex flex-col gap-20 justify-end">
-                    <div className="z-10 flex flex-col md:flex-col tracking-tighter leading-none gap-4 max-w-[700px]">
-
-                        <h2 className=" text-[32px] md:text-[64px] tracking-tighter leading-none uppercase ">
+                    <div className="z-10 flex flex-col xl:flex-col tracking-tighter leading-none gap-4 2xl:max-w-[700px] ">
+                        <h2 className=" text-[32px] sm:text-[50px] md:text-[64px] tracking-tighter leading-none uppercase flex flex-col">
                             Choose & Trade
-                            <span className="z-20 relative inline-block gap-2  ">
+                            <span className="z-20 relative inline-block gap-2 w-fit ">
                                 Ready-To-Go Strategies
                                 <Image
                                     src="/images/regular.png"
                                     alt="photo of regular"
+                                    loading="lazy"
                                     width={80}
                                     height={80}
-                                    className="z-10 absolute right-0 top-6 md:-right-20 md:top-10 -translate-y-1/2 rotate-[-25deg] pointer-events-none"
+                                    className="z-10 absolute -right-0 -top-8 sm:-right-20 sm:top-7 -translate-y-1/2 rotate-[-25deg] pointer-events-none"
                                 />
                             </span>
                         </h2>
@@ -54,10 +54,10 @@ export default function Hero() {
                                     <RightArrow />
                                 </Link>
                                 <Link href="https://apps.apple.com/au/app/blackwell-invest/id1666036351" target="_blank" rel="noopener noreferrer" className="relative cta-register group flex flex-row items-center gap-2 w-fit">
-                                    <Image src="/images/app-store.jpg" alt="image of apple" width={100} height={100} className="object-cover"/>
+                                    <Image src="/images/appstore/app-store.jpg" alt="image of apple" width={100} height={100} className="object-cover"/>
                                 </Link>
                                 <Link href="https://play.google.com/store/apps/details?id=com.BlackwellGlobalInvestmentsUKLimited.pelican&hl=en_SG" target="_blank" rel="noopener noreferrer" className="relative cta-register group flex flex-row items-center gap-2 w-fit">
-                                     <Image src="/images/google-play.jpg" alt="image of apple" width={100} height={100} className="object-cover"/>
+                                     <Image src="/images/appstore/google-play.jpg" alt="image of apple" width={100} height={100} className="object-cover"/>
                                 </Link>
                             </div>
 
