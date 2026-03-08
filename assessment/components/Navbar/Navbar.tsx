d8aa5b8d046e8b4e5093b0ca4d0fb19658977dd1
xinhan <xhxhan00@gmail.com>
2026-03-08 12:59:09 +0800

    remove spaces

--- assessment/components/Navbar/Navbar.tsx
+++ assessment/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import RightArrow from "../IconSVG/Arrow"
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-999 flex justify-center px-[5%] justify-center items-center">
+        <nav className="sticky top-0 z-999 flex justify-center px-[5%] justify-center items-center ">
             <div className="w-[1520px] flex flex-row justify-between py-8">
                 <h2 className="flex justify-center items-center">BLACKELL GLOBAL</h2>
                 <Link href="/" className="cta-register group flex flex-row items-center gap-2">
