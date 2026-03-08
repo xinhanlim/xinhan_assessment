f7854d5ef04baba6167dc02c1a978384a540ece6
xinhan <xhxhan00@gmail.com>
2026-03-08 20:20:04 +0800

    create slidesection componet

---
+++ assessment/components/Install/SlideSection.tsx
@@ -0,0 +1,20 @@
+export default function SlideSection() {
+
+    const linkData = [
+        { title: 'Install our app, “Blackwell Invest"', image: "/images/guide/guide-1.png" },
+        // { title: 'Login OR create a new account', image: "/images/guide/guide-2.png" },
+        // { title: 'Click “Account”', image: "/images/guide/guide-3.png" },
+        // { title: 'Select “BlackwellGlobalAsia-Live” server', image: "/images/guide/guide-4.png" },
+        // { title: 'Fill in your Blackwell Global trading account OR create a new account', image: "/images/guide/guide-4.png" },
+        // { title: 'Click “Copy Trades” ', image: "/images/guide/guide-4.png" },
+        // { title: 'Click “Done”', image: "/images/guide/guide-4.png" },
+    ]
+
+    return (
+        <section className="flex justify-center w-full h-full items-center ">
+            <div className="relative w-full h-screen installSection">
+        
+            </div>
+        </section>
+    )
+}
