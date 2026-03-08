3cdc6406680b35707ac79befece689e7ea9c1ee4
xinhan <xhxhan00@gmail.com>
2026-03-08 12:58:48 +0800

    updated font style

--- assessment/components/Banner/Banner.tsx
+++ assessment/components/Banner/Banner.tsx
@@ -5,7 +5,7 @@
 export default function Banner() {
     return (
         <div className="overflow-hidden  ">
-            <div className="flex bannerCarousel gap-10 whitespace-nowrap tracking-tighter leadning-none">
+            <div className="flex bannerCarousel gap-10 whitespace-nowrap tracking-tighter leadning-none text-[40px] md:text-[64px] py-4 md:py-[16px]">
                 {Array(20).fill("BLACKWELL INVESTMENT").map((text, i) => (
                     <span key={i} className="">
                         {text}
