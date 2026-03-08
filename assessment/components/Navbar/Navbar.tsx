2fb104ab22c96396e6520e53700652856a0cb209
xinhan <xhxhan00@gmail.com>
2026-03-08 19:32:46 +0800

    added text-16px

--- assessment/components/Navbar/Navbar.tsx
+++ assessment/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <nav className="sticky top-0 z-999 flex justify-center px-[5%] justify-center items-center ">
             <div className="w-[1520px] flex flex-row justify-between py-8">
                 <h2 className="flex justify-center items-center">BLACKELL GLOBAL</h2>
-                <Link href="/" className="cta-register group flex flex-row items-center gap-2">
+                <Link href="/" className="cta-register group flex flex-row items-center gap-2 text-[16px] ">
                     REGISTER
                     <RightArrow/>
                 </Link>
