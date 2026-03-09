016c9854afa1993185075541c07844357fa03395
xinhan <xhxhan00@gmail.com>
2026-03-09 23:09:18 +0800

    learn about async for promise and applied

--- assessment/store/authStore.tsx
+++ assessment/store/authStore.tsx
@@ -4,12 +4,14 @@ interface User {
     firstName: string;
     lastName: string;
     email: string;
+    password: string;
 }
 
 interface AuthState {
     open: boolean;
     mode: "login" | "signup";
     user: User | null;
+    users: User[];
     openModal: (mode?: "login" | "signup") => void;
     closeModal: () => void;
     toggleModal: () => void;
@@ -22,29 +24,50 @@ export const useAuth = create<AuthState>((set, get) => ({
     open: false,
     mode: "login",
     user: null,
+    users: [],
     openModal: (mode = "login") => set({ open: true, mode }),
     closeModal: () => set({ open: false }),
     toggleModal: () => set((state) => ({ mode: state.mode === "login" ? "signup" : "login" })),
 
-    login: (email, password) => new Promise<void>((resolve, reject) => {
-        setTimeout(() => {
-            if (email && password) {
-                resolve();
-            } else {
-                reject(new Error("invalid credentials"));
-            }
-        }, 1000)
-    }),
-    signup: (data) =>
-        new Promise<void>((resolve, reject) => {
-            setTimeout(() => {
-                if (data.password === data.confirmPassword) {
-                    set({ user: { firstName: data.firstName, lastName: data.lastName, email: data.email }, open: false });
-                    resolve();
-                } else {
-                    reject(new Error("Passwords do not match"));
-                }
-            }, 1000);
-        }),
-    logout : () => set ({user : null}),
-}))
+    login: async (email, password) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        const {users} = get();
+        const existingUser = users.find((u)=>u.email === email)
+
+        if(!existingUser){
+            throw new Error("Invalid Email/Password")
+        }
+
+        if(existingUser.password !== password){
+            throw new Error("Invalid Email/Password")
+        }
+
+        set({
+            user: existingUser,
+            open: false
+        });
+    },
+
+    signup: async (data) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        const { users } = get();
+        if (users.find((u) => u.email === data.email)) {
+            throw new Error("Email Already Registered")
+        }
+
+        const newUser= {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            password: data.password,
+        }
+
+        set({
+            users: [...users, newUser],
+            user: newUser,
+            open: false
+        });
+    },
+    logout: () => set({ user: null }),
+}));
