2cd1e9df3d5a366dcfc913c303166182f7ba9619
xinhan <xhxhan00@gmail.com>
2026-03-10 23:20:05 +0800

    update css for bannercarousel

--- assessment/components/Banner/Banner.tsx
+++ assessment/components/Banner/Banner.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-
-
 export default function Banner() {
     return (
         <div className="overflow-hidden  ">
-            <div className="flex bannerCarousel gap-10 whitespace-nowrap tracking-tighter leadning-none text-[40px] md:text-[64px] py-4 md:py-[16px]">
+            <div className="flex bannerCarousel gap-10 whitespace-nowrap tracking-tighter leading-none text-[64px] py-4">
                 {Array(20).fill("BLACKWELL INVESTMENT").map((text, i) => (
                     <span key={i} className="">
                         {text}
