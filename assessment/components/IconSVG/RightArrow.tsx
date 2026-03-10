94476e022ba1cfae8ef473f74be5c8ee2b922b1b
xinhan <xhxhan00@gmail.com>
2026-03-10 23:20:30 +0800

    input right and left arrow for slideshow

---
+++ assessment/components/IconSVG/RightArrow.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RightArrow: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M9 6l6 6-6 6" />
+  </svg>
+);
+
+export default RightArrow;
