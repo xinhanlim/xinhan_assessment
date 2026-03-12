aa21c1130e54dd438a2eda1401a9481eb82a8b90
xinhan <xhxhan00@gmail.com>
2026-03-12 21:05:11 +0800

    inserted datascham ( countries ) to forms

--- assessment/components/Auth/SignupForm.tsx
+++ assessment/components/Auth/SignupForm.tsx
@@ -1,8 +1,9 @@
 import { useForm } from "react-hook-form";
 import { useAuth } from "../../store/authStore"
 import toast from "react-hot-toast";
-import { SignupData, signupSchema } from "./dataSchema";
+import { SignupData, signupSchema, countries } from "./dataSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 
 interface signupProps {
     closeModal: () => void;
@@ -10,7 +11,7 @@ interface signupProps {
 }
 
 export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
-    const { signup, toggleModal } = useAuth()
+    const { signup, toggleModal, } = useAuth()
     const { register, handleSubmit, formState: { errors } } = useForm<SignupData>({
         resolver: zodResolver(signupSchema)
     })
@@ -35,11 +36,13 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
     return (
         <div className=" relative w-full flex flex-col h-full overflow-hidden">
             <div className="flex flex-row justify-between items-center px-[4%]">
-                <p className="text-[32px]">SIGN UP FORM</p>
+                <div className="relative w-[120px] h-[100px] sm:w-[120px] sm:h-[80px] ">
+                    <Image src="/images/BWlogo.png" alt="logo image" fill className="object-cover w-full h-full" />
+                </div>
                 <button onClick={closeModal} className="closeBtn cursor-pointer"> x </button>
             </div>
             <div className="overflow-y-auto flex-1">
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8 px-[4%] py-8 formText">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8 px-[4%] py-8 formText ">
                     <div className="flex flex-col sm:flex-row gap-8">
                         <div className="flex flex-col w-full gap-2 h-fit">
                             <p>
@@ -48,7 +51,7 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                     {errors.firstName?.message && `* ${errors.firstName?.message}`}
                                 </span>
                             </p>
-                            <input {...register("firstName")} placeholder="First Name" className={`rounded-sm border p-2 ${errors.firstName ? "border-red-500" : "border-black"}`} />
+                            <input {...register("firstName")} placeholder="First Name" className={`loginInput ${errors.firstName ? "border-red-500" : "border-black"}`} />
 
                         </div>
                         <div className="flex flex-col w-full gap-2 h-fit">
@@ -58,7 +61,7 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                     {errors.lastName?.message && `* ${errors.lastName?.message}`}
                                 </span>
                             </p>
-                            <input {...register("lastName")} placeholder="Last Name" className={`rounded-sm border p-2 ${errors.lastName ? "border-red-500" : "border-black"}`} />
+                            <input {...register("lastName")} placeholder="Last Name" className={`loginInput ${errors.lastName ? "border-red-500" : "border-black"}`} />
 
                         </div>
                     </div>
@@ -70,7 +73,7 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                 {errors.email?.message && `* ${errors.email?.message}`}
                             </span>
                         </p>
-                        <input {...register("email")} placeholder="Email" className={`rounded-sm border p-2 ${errors.email ? "border-red-500" : "border-black"}`} />
+                        <input {...register("email")} placeholder="Email" className={`loginInput ${errors.email ? "border-red-500" : "border-black"}`} />
 
                     </div>
 
@@ -81,13 +84,28 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                 {errors.mobile?.message && `* ${errors.mobile?.message}`}
                             </span>
                         </p>
-                        <input {...register("mobile")} placeholder="Mobile Number" className={`rounded-sm border p-2 ${errors.mobile ? "border-red-500" : "border-black"}`} />
+                        <input {...register("mobile")} placeholder="Mobile Number" className={`loginInput ${errors.mobile ? "border-red-500" : "border-black"}`} />
 
                     </div>
 
                     <div className="flex flex-col w-full gap-2 h-fit">
-                        <p>Country: </p>
-                        <input {...register("country")} placeholder="Country" className="border p-2" />
+                        <p>
+                            *Country:
+                            <span className="px-2 text-red-500 text-[14px] ">
+                                {errors.country?.message && `* ${errors.country?.message}`}
+                            </span>
+                        </p>
+                        <select
+                            {...register("country")}
+                            className={`navigateFormPadd ${errors.country ? "border-red-500" : "border-black"}`}
+                            defaultValue="">
+                            <option className="text-grey-400" value="" disabled>
+                                Select a country
+                            </option>
+                            {countries.map(c => (
+                                <option key={c} value={c} className="text-[#282828]">{c}</option>
+                            ))}
+                        </select>
                     </div>
 
                     <div className="flex flex-col w-full gap-2 h-fit">
@@ -97,7 +115,7 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                 {errors.password?.message && `* ${errors.password?.message}`}
                             </span>
                         </p>
-                        <input {...register("password")} placeholder="Password" className={`rounded-sm border p-2 ${errors.password ? "border-red-500" : "border-black"}`} />
+                        <input {...register("password")} placeholder="Password" className={`loginInput ${errors.password ? "border-red-500" : "border-black"}`} />
 
                     </div>
 
@@ -108,7 +126,7 @@ export default function SignupForm({ closeModal, closeOverlay }: signupProps) {
                                 {errors.confirmPassword?.message && `* ${errors.confirmPassword.message}`}
                             </span>
                         </p>
-                        <input {...register("confirmPassword")} placeholder="Password" className={`rounded-sm border p-2 ${errors.confirmPassword ? "border-red-500" : "border-black"}`} />
+                        <input {...register("confirmPassword")} placeholder="Password" className={`loginInput ${errors.confirmPassword ? "border-red-500" : "border-black"}`} />
 
                     </div>
                     <button type="submit" className="bg-black-200 px-4 py-4 ">Sign Up</button>
