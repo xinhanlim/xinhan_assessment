d887cce9efee52256bb6afc9376af2566fa99277
xinhan <xhxhan00@gmail.com>
2026-03-12 21:08:19 +0800

    compile some tailwind css to css rules.

--- assessment/store/authModal.tsx
+++ assessment/store/authModal.tsx
@@ -18,7 +18,7 @@ export default function AuthModal({closeOverlay}: AuthModalProps) {
         <div className="fixed inset-0 bg-black/95 z-999 cursor-pointer flex justify-center items-center">
             <div className="relative w-[1520px] h-screen flex flex-col">
                 <div className="flex justify-center w-full h-full items-center cursor-default px-[5%] sm:px-[20%] py-[5%] sm:py-[8%]">
-                    <div className="w-full h-full bg-white rounded-md px-4 py-4">
+                    <div className="w-full h-full loginBg">
                         {mode === "login" ? <LoginForm closeModal={closeModal} closeOverlay={closeOverlay}/> : <SignupForm closeModal={closeModal} closeOverlay={closeOverlay}/>}
                     </div>
                 </div>
