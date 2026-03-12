6de2e8268a23f92ca8a5fe127237015b0c79b384
xinhan <xhxhan00@gmail.com>
2026-03-12 21:01:46 +0800

    replace count string to enum for options

--- assessment/components/Auth/dataSchema.tsx
+++ assessment/components/Auth/dataSchema.tsx
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
+
+export const countries = ["Singapore", "Malaysia", "Indonesia"] as const;
+
 export const signupSchema = z.object({
     firstName: z.string().nonempty("First Name is Required"),
     lastName: z.string().nonempty("Last Name is Required"),
     email: z.string().email("Invalid Email").nonempty("Email is Required"),
     mobile: z.string().nonempty("Mobile Number is Required").regex(/^\d{8,15}$/, "Mobile number must be 8-15 digits"),
-    country: z.string().optional(),
+    country: z.enum(countries, { message: "Please select a country" }),
     password: z.string().min(8, "Password must be 8 characters long"),
     confirmPassword: z.string().min(8, "Password must matched"),
 }).refine((data) => data.password === data.confirmPassword, {
